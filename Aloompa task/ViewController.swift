2ab68495523943e806230caac44ce064708f8765
Aidan <aidan.gutierrez@pop.belmont.edu>
2019-08-01 19:15:07 -0500

    Initial Commit

---
+++ Aloompa task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aloompa task
+//
+//  Created by Aidan Gutierrez on 8/1/19.
+//  Copyright © 2019 Aidan Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
